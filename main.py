faf0367ecd55a95ec9d0e88ce5f5525c5ad5850a
Ishaan Jain <ishaan160799@gmail.com>
2025-04-13 23:38:09 +0530

    agent and agent_executor created

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -38,3 +39,12 @@ prompt = ChatPromptTemplate.from_messages(
         ("placeholder", "{agent_scratchpad}"),
     ]
 ).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools=[])
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
+print(raw_response)
+
+structured_response = parser.parse(raw_response.get("output"))
+print(structured_response)
